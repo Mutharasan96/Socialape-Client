c4d6b64a720011a878a8413047bfed0de0b9fa5d
Mutharasan <muthu.ct.96@gmail.com>
2020-08-24 06:20:05 +0530

    created initial stages of the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MuiThemeprovider from "@material-ui/core/styles/MuiThemeProvider";
+import createTheme from "@material-ui/core/styles/createMuiTheme";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+
+const theme = createMuiTheme({
+  primary: {
+    light: '#33c9dc',
+    main: '#00bcd4',
+    dark: '#008394',
+    contrastText: '#fff'
+  },
+  secondary: {
+    light: '#ff6333',
+    main: '#ff3d00',
+    dark: '#b22a00',
+    contrastText: '#fff'
+  },
+})
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
